1415bf215441f2d9a55dccf62491d40fc34271ac
leonpeng <leon.peng@live.com>
2016-09-02 17:04:41 +0800

    ...

---
+++ src/leon2012/phpapi/Config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 
+ * @authors Leon Peng (xingskycn@163.com)
+ * @date    2016-09-02 17:03:51
+ * @version $Id$
+ */
+
+namespace leon2012\phpapi;
+
+class Config 
+{
+    
+    public function __construct()
+    {
+        
+    }
+}

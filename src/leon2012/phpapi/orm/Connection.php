44892f10a45d7f39c104658cbb442c6e5e58ed4c
PengLeon <xingskycn@163.com>
2016-09-16 17:07:47 +0800

    ...

---
+++ src/leon2012/phpapi/orm/Connection.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @Author: PengLeon
+ * @Date:   2016-09-16 16:37:01
+ * @Last Modified by:   PengLeon
+ * @Last Modified time: 2016-09-16 16:47:43
+ */
+
+namespace leon2012\phpapi\orm;
+
+class Connection 
+{
+
+	private $_config;
+	private $_driver;
+
+	public function __construct($config=[])
+	{
+
+	}
+
+	
+}
---
+++ src/leon2012/phpapi/orm/drivers/PDODriver.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @Author: pengleon
+ * @Date:   2016-09-16 16:34:44
+ * @Last Modified by:   PengLeon
+ * @Last Modified time: 2016-09-16 16:36:32
+ */
+
+class PDODriver
+{
+	
+}
